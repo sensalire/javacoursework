45afe078d3252f8b823742a34761d36b25e3c1a7
sensalire <117361979+sensalire@users.noreply.github.com>
2022-11-17 10:38:11 +0300

    Add files via upload

---
+++ Registration/Registration.java
@@ -0,0 +1,12 @@
+package Registration;
+
+import javax.swing.JFrame;
+
+class Registration {
+
+	    public static void main(String[] args)
+	                          throws Exception
+	    {
+	        JFrame f = new MyFrame();
+	    }
+	}
